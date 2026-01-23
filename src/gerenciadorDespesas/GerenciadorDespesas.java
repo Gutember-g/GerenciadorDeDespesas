820112c41ee6d4b378f2b3ec69d4df73f0b320b9
Gutemberg Batista da Silva <guter.silva@hotmail.com>
2026-01-23 20:08:47 -0300

    Criação do metodo Relatorio Semanal

--- src/gerenciadorDespesas/GerenciadorDespesas.java
+++ src/gerenciadorDespesas/GerenciadorDespesas.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class GerenciadorDespesas {
 	static String[] categorias = {"Alimentacao", "Transporte", "Lazer", "Outros"};
+	static String[] diasSemana = {"Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado", "Domingo"};
 	static double[] gastosSemana = new double[7]; //7 dias da semana
-	double orcamentoMensal = 2000.0;
+	static double orcamentoMensal = 2000.0;
 	static int dSemana = 0;
 	static int categoria = 0;
 	
@@ -51,12 +52,48 @@ public class GerenciadorDespesas {
 	
 	private static void relatorioSemana() {
 		// TODO Auto-generated method stub
+		double total = 0;
+		double maior = 0;
+		int dia = -1;
+		for(int i = 0; i < gastosSemana.length; i++) {
+			double gastos = gastosSemana[i];
+			total += gastos;
+			
+			if(gastos > maior) {
+				maior = gastos;
+				dia = i;
+			}
+		}
 		
-	}
+		System.out.println("Total gasto na semana = " + total);
+		
+		
+		if(total > orcamentoMensal * 0.8) {
+			System.out.println("Voce passou de 80% do orçamento!!");
+		}
+		if(dia != -1) {
+		System.out.println("Maior gasto: R$" + maior + " foi na " + diasSemana[dia]);
+	}}
 
 	private static void relatorioDia() {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("==== Selecione o dia da Semana =====");
+		System.out.println("1 - Segunda-Feira");
+		System.out.println("2 - Terca-Feira");
+		System.out.println("3 - Quarta-Feira");
+		System.out.println("4 - Quinta-Feira");
+		System.out.println("5 - Sexta-Feira");
+		System.out.println("6 - Sabado");
+		System.out.println("7 - Domingo");
+		dSemana = s.nextInt() -1;
 		
+		System.out.println("================");
+		
+		System.out.println("Gastos do dia = " + gastosSemana[dSemana]);
+		if(gastosSemana[dSemana] > 150) {
+			System.out.println("Gastos Altos Hoje!!");
+		}
 	}
 
 	private static void registraGasto() {
